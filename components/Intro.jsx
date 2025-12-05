0a05ae1ee7725703fc4a9c1530bb6de829c86394
sharath-98 <sharathchander.p@gmail.com>
2025-12-05 10:44:18 -0500

    Nav and logo page upds.

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -1,48 +1,92 @@
-"use client"
-import React from 'react'
-import Image from 'next/image'
-import { TypeAnimation } from 'react-type-animation'
-import {Link as ScrollLink} from 'react-scroll'
-
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import { Link as ScrollLink } from "react-scroll";
+import RotatingShape from "./RotatingShape";
+import Header from "./Header";
 
 const Intro = () => {
-  return (
-			<section className="h-[800px] relative bg-accent/10 xl:bg-white">
-				{/* {Header } */}
-				<div>Header</div>
-				<div className="container mx-auto h-full">
-					<div className="relative z-20 h-full w-full xl:max-w-[768px] flex flex-col items-center xl:items-center justify-center text-center xl:text-left pt-10">
-						<h1 className="h1 mb-2 max-w-[320px] xl:max-w-none">
-							<span className="text-accent">I Architect And</span> Build Powerful
-							<TypeAnimation
-								preRenderFirstString={true}
-								sequence={[
-									" Applications",
-									2000,
-									" AI/ML Models",
-									2000,
-									" Cloud Soln.",
-									2000
-								]} repeat={Infinity}
-                                speed={50}
-                                wrapper='span'
-                                cursor={false}
-                                className='ml-2 xl:ml-4'
-							/>
-						</h1>
-                        <p className='lead max-w-[476px] mb-7'>
-                            Delivering powerful, scalable and performant products.
-                        </p>
-                        <ScrollLink to="contact" smooth>
-                            <button className='btn btn-accent mb-8'>Contact me</button>
-                        </ScrollLink>
-                        
-                        {/* {Stats} */}
-                        <div>Stats</div>
+	return (
+		<section className="h-[800px] relative bg-accent/10 xl:bg-white">
+			<Header />
+			<div className="container mx-auto h-full">
+				<div className="relative z-20 h-full w-full xl:max-w-[768px] flex flex-col items-center xl:items-center justify-center text-center xl:text-left pt-10">
+					<h1 className="h1 mb-2 max-w-[320px] xl:max-w-none">
+						<span className="text-accent">I Architect And</span> Build Powerful
+						<TypeAnimation
+							preRenderFirstString={true}
+							sequence={[
+								" Applications",
+								2000,
+								" AI/ML Models",
+								2000,
+								" Cloud Soln.",
+								2000,
+							]}
+							repeat={Infinity}
+							speed={50}
+							wrapper="span"
+							cursor={false}
+							className="ml-2 xl:ml-4"
+						/>
+					</h1>
+					<p className="lead max-w-[476px] mb-7">
+						Delivering powerful, scalable and performant products.
+					</p>
+					<ScrollLink to="contact" smooth>
+						<button className="btn btn-accent mb-8">Contact me</button>
+					</ScrollLink>
+
+					{/* {Stats} */}
+					<div>Stats</div>
+				</div>
+				<div className="hidden xl:flex w-[55vw] h-[800px] absolute top-0 right-0 bg-accent ">
+					<div className="absolute w-[558px] h-[642px] bottom-0 z-40 left-[6.5vw]">
+						<Image
+							src="/assets/hero/ProfilePic-3.png"
+							fill
+							quality={100}
+							priority
+							className="object-contain"
+						/>
+					</div>
+
+					{/* {arrow shape} */}
+					<div className="hidden xl:flex absolute top-40 left-[4vw]">
+						<Image src="/assets/hero/arrow.svg" width={160} height={160} />
+					</div>
+
+					{/* {shape 1} */}
+					<div className="absolute top-[600px] left-[3vw]">
+						<RotatingShape
+							content={<Image src="assets/hero/shape-1.svg" width={38} height={38} />}
+							direction="left"
+							duration={6}
+						/>
+					</div>
+
+					{/* {shape 2} */}
+					<div className="absolute top-[240px] xl:left-[30vw]">
+						<RotatingShape
+							content={<Image src="assets/hero/shape-2.svg" width={34} height={34} />}
+							direction="right"
+							duration={5}
+						/>
+					</div>
+
+					{/* {shape 3} */}
+					<div className="absolute top-[480px] xl:left-[40vw]">
+						<RotatingShape
+							content={<Image src="assets/hero/shape-3.svg" width={36} height={36} />}
+							direction="left"
+							duration={7}
+						/>
 					</div>
 				</div>
-			</section>
-		);
-}
+			</div>
+		</section>
+	);
+};
 
-export default Intro
+export default Intro;
---
+++ components/Nav.jsx
@@ -0,0 +1,37 @@
+"use client"
+import React from 'react'
+import {Link as ScrollLink} from 'react-scroll'
+
+const links = [
+	{
+		name: "home",
+	},
+	{
+		name: "about",
+	},
+	{
+		name: "journey",
+	},
+	{
+		name: "work",
+	},
+	{
+		name: "contact",
+	},
+];
+
+const Nav = ({containerStyles, listStyles, LinkStyles, spy}) => {
+  return (
+    <nav className={containerStyles}>
+        <ul className={listStyles}>
+            {
+                links.map((link, index) => {
+                    return <ScrollLink spy={spy} key={index} activeClass="active" to={link.name} smooth className={LinkStyles}>{link.name}</ScrollLink>
+                })
+            }
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
