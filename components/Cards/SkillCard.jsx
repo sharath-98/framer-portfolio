e474afcf0019992b92830f9f141523d00de504e5
sharath-98 <sharathchander.p@gmail.com>
2025-12-13 01:47:19 -0500

    skill upds.

--- components/Cards/SkillCard.jsx
+++ components/Cards/SkillCard.jsx
@@ -6,16 +6,16 @@ const SkillCard = ({ icon, name, description, bg, shadow }) => {
 	return (
 		<div>
 			<div
-				className={`w-full h-[300px] p-8 rounded-[30px] flex items-center justify-center mb-6 relative overflow-hidden bg-accent hover:bg-primary/30 text-[#ffffff]  transition-all duration-200`}
+				className={`w-full h-[300px] p-8 rounded-[30px] flex items-center justify-center mb-6 relative overflow-hidden bg-accent hover:bg-primary/30 text-[#ffffff] hover:text-[#ffffff] transition-all duration-200`}
 			>
 				<Badge className="bg-primary text-base z-40 absolute top-6 left-6 capitalize">
 					{name}
 				</Badge>
-				<div className="items-center justify-center flex flex-col">
+				<div className="items-center justify-center flex flex-col text-secondary hover:text-[#ffffff]">
 					<div className="bg-white rounded-full p-3 w-[70px] h-[70px] transition-all duration-200 top-12 left-12 mb-5 text-white">
 						<Image src={icon} width={70} height={70} alt="" />
 					</div>
-					<h4 className="h4 text-primary">{description}</h4>
+					<h4 className="">{description}</h4>
 				</div>
 			</div>
 		</div>
