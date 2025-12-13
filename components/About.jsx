9b4e44a50679b06a3a03124cea689cf80fed8a1d
sharath-98 <sharathchander.p@gmail.com>
2025-12-13 08:56:38 -0500

    Profile Pic upds.

--- components/About.jsx
+++ components/About.jsx
@@ -16,11 +16,11 @@ const About = () => {
 							{/* About me Image */}
 							<div className="rounded-tl-[8px] rounded-tr-[120px] w-full bg-[#e5f8f6] min-h-[480px] flex items-end justify-center">
 								<Image
-									src="/assets/about/ProfilePic-3.png"
-									width={350}
-									height={478}
+									src="/assets/about/ProfilePic-4.png"
+									className="object-cover rounded-tr-[120px]"
 									quality={100}
 									priority
+									fill
 									alt=""
 								/>
 							</div>
